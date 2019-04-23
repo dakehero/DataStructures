f26923201b24943ff5475a71e25ac4c6d7554e89
全伟 <dake_hero@hotmail.com>
2019-04-23 11:39:19 +0800

    fix directory

---
+++ DataSturcture.Tree/BinaryThreadTree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.Tree
+{
+    public class BinaryThreadTreeNode<T> : BinaryTreeNode<T>
+    {
+        public BinaryThreadTreeNode() : base() { }
+        public BinaryThreadTreeNode(T data,Bina)
+    }
+}
